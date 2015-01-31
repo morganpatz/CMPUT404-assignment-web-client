97f0363bc77a95c57cd4eab6fe9622e101e94197
morganpatz <patzelt@ualberta.ca>
2015-01-31 14:37:47 -0700

    Some progress

--- httpclient.py
+++ httpclient.py
@@ -23,6 +23,9 @@ import socket
 import re
 # you may use urllib to encode data appropriately
 import urllib
+from urlparse import urlparse
+import string
+from StringIO import StringIO
 
 def help():
     print "httpclient.py [GET/POST] [URL]\n"
@@ -31,6 +34,9 @@ class HTTPRequest(object):
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
+    
+
+        
 
 class HTTPClient(object):
     #def get_host_port(self,url):
@@ -46,25 +52,58 @@ class HTTPClient(object):
         print ("Socket created successfully")  
         
         try:
-            remote_ip = socket.gethostbyname(host)
-        except socket.gaierror: # gai = get address ip
+            #remote_ip = socket.gethostbyname(host)
+            parse = urlparse(host)
+            remote_ip = parse.netloc
+            remote_ip = string.split(remote_ip, ":", 1)[0]
+            print "Remote IP: " + remote_ip
+        
+        except urlparse.error:
             print("Host name could not be resolved")
             sys.exit()
-        print ("IP address of " + host + " is " + remote_ip)
         
-        s.connect((remote_ip, port)) # need both brackets
-        print ("Socket connected to " + host + " on IP " + remote_ip)        
+        try:
+            s.connect((remote_ip, port)) # need both brackets
+            print ("Socket connected to " + host + " on IP " + remote_ip) 
+        except socket.error as msg:
+            print ("Failed to connect")
+            print ("Error Code: " + str(msg[0]) + ", Error Message: " + str(msg[1]))
+            sys.exit()
         
+    
+    
+    def get_host(self, url):
+        words = string.split(self.url, "/")
+        host = words[2]
+        return host
+            
+    def get_path(self, url):
+        words = string.split(self.url, "/")
+        path = ""
+        for x in range (3, len(words)):
+            path = path + "/" + words[x]
+        return path
+
+    def print_requestline(self):
+        requestline = "%s %s HTTP/1.1\r\n" % (self.method, self.get_path())
+        self.data = self.data + requestline
+    
+    def print_header(self, key, value):
+        header = "%s: %s\r\n" % (key, value)
+        self.data = self.data + header
+    
+    def end_headers(self):
+        self.data = self.data + "\r\n"
+    
+    def print_body(self):
+        self.data = self.data + self.body
         
-
-    def get_code(self, data):
-        return None
-
-    def get_headers(self,data):
-        return None
-
-    def get_body(self, data):
-        return None
+    def send(self):
+        self.print_requestline()
+        self.print_header("Host", self.get_host())
+        self.end_headers()
+        self.print_body()
+        s.sendall(self.data + "\n")    
 
     # read everything from the socket
     def recvall(self, sock):
@@ -82,25 +121,43 @@ class HTTPClient(object):
         code = 500
         body = ""
         host = url
-        port = 80
-        self.connect(host, port)        
-        return HTTPRequest(code, body)
+        base = string.split(url, "/")[2]
+        port = int(string.split(base, ":")[1])
+        try:
+            self.connect(host, port) 
+            code = 200
+            print "Connected"
+        except:
+            code = 500
+            
+        HTTPRequest("GET", url, code, body).send()
 
     def POST(self, url, args=None):
         code = 500
         body = ""
         host = url
-        port = 8080
-        self.connect(host, port)
-        return HTTPRequest(code, body)
+        base = string.split(url, "/")[2]
+        port = int(string.split(base, ":")[1])
+        print "URL: " + url
+    
+        try:
+            self.connect(host, port) 
+            code = 200
+            print "Connected"
+        except:
+            code = 500
+            
+        HTTPRequest("POST", url, code, body).send()
 
     # gets called first
     # tests to see if it is a POST or GET request
     # calls .POST or .GET respectively
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
+            print "POST"
             return self.POST( url, args )
         else:
+            print "GET"
             return self.GET( url, args )
     
 if __name__ == "__main__":
